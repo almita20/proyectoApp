8fe393547c9e6b0ab4f48f43cdc6e022b486eec9
proyectoAlmayT <tobiassgomez442@gmail.com>
2023-05-18 13:05:32 -0300

    carpetas

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export default{
+    apiKey: "AIzaSyCnQmJQhW1oVw_s_N3cREvKJIsenetTo-0",
+    authDomain: "ricero-8183e.firebaseapp.com",
+    projectId: "ricero-8183e",
+    storageBucket: "ricero-8183e.appspot.com",
+    messagingSenderId: "125704921890",
+    appId: "1:125704921890:web:3fdf6f0655775232a67b7b",
+    measurementId: "G-MY6PJ1V5RV"
+  }
