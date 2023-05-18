30f4fa9e973e514438d17ad1011369b8b845097a
proyectoAlmayT <tobiassgomez442@gmail.com>
2023-05-18 13:53:28 -0300

    form

--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -1,1 +1,24 @@
-MediaStreamAudioDestinationNodeasdad
+import { useForm } from "react-hook-form";
+
+const Formulario = () => {
+    return <div>
+        <h2>RiCero</h2>
+        <h3>Disminuye riesgos escolares</h3>
+        <form>
+        <div>
+            <label > Nombre de usuario</label>
+            <input type="text" name="Nombre"/>
+        </div>
+        <div>
+            <label >Correo electronico</label>
+            <input type="text" name="Correo"/>
+        </div>
+        <div>
+            <label>Contraseña </label>
+            <input type="password" name="Contraseña" />
+        </div>
+        </form>
+    </div>
+}
+
+export default Formulario;
