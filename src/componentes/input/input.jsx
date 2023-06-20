51a13ac350cab549028ad54457666821297452fa
proyectoAlmayT <alma.beroisa.epet20@gmail.com>
2023-06-20 20:40:47 -0300

    div y comienzo de form

--- src/componentes/input/input.jsx
+++ src/componentes/input/input.jsx
@@ -1,3 +1,9 @@
 export function input(props){
-    
+    return(
+        <div classname="input">
+        <form action="/procesar_formulario" method="post">
+       
+        </form>
+        </div>
+    )
 }
