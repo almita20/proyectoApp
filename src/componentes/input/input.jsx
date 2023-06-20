30d81da9fcdedd03fffc889cb6382981ec6b72b5
proyectoAlmayT <alma.beroisa.epet20@gmail.com>
2023-06-20 20:41:13 -0300

    primer input de nombre de usuario

--- src/componentes/input/input.jsx
+++ src/componentes/input/input.jsx
@@ -2,7 +2,8 @@ export function input(props){
     return(
         <div classname="input">
         <form action="/procesar_formulario" method="post">
-       
+        <input type="text" id="nombre" name="nombre" placeholder="Nombre de Usuario">
+        
         </form>
         </div>
     )
