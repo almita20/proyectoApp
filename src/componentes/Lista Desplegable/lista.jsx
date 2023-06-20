52dfc8816723a616712b47ea169894847e7f9ec6
proyectoAlmayT <alma.beroisa.epet20@gmail.com>
2023-06-20 20:53:24 -0300

    option mi perfil|

--- src/componentes/Lista Desplegable/lista.jsx
+++ src/componentes/Lista Desplegable/lista.jsx
@@ -2,8 +2,8 @@ export function lista(props){
     return(
         <div className="lista">
         <select name="opciones">
+        <option value="mi perfil">Mi perfil</option>
         
-
         </div>
     )
 }
