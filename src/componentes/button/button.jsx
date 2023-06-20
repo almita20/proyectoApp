301fe71dabb5590e6a79d01bb9a2d314ed72f507
proyectoAlmayT <alma.beroisa.epet20@gmail.com>
2023-06-20 20:32:30 -0300

    comienzo estructura button

--- src/componentes/button/button.jsx
+++ src/componentes/button/button.jsx
@@ -1,13 +1,5 @@
-export function Button(props){
-    return(
-            <div className="button">
-            <button style={
-            {
-                backroundColor: props.bgcolor,
-            }
-            }>
-            {props.text}
-            </button>
-            </div>
-    )
+export function Button (props){
+    return 
+
+
 }
