e1b9c25c919f60d62521a2f0c489c8d370d928eb
proyectoAlmayT <alma.beroisa.epet20@gmail.com>
2023-06-20 21:13:15 -0300

    importamos la funcion de appCSS

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import {Button} from './componentes/button/button.jsx';
 import {input} from './componentes/input/input.jsx';
 import {lista} from './componentes/Lista Desplegable/lista.jsx';
 import {planilla} from './componentes/Planilla Asistencia/planilla.jsx';
-
+import './App.css';
 function App(){
     return(
     <div className="App">
