ce6b96af40117dd15a3ea8ceac8fd36d6f43c8af
proyectoAlmayT <alma.beroisa.epet20@gmail.com>
2023-06-20 21:11:59 -0300

    importamos la funcion de la lista desplegable

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import {Button} from './componentes/button/button.jsx';
 import {input} from './componentes/input/input.jsx';
+import {lista} from './componentes/Lista Desplegable/lista.jsx';
 
 function App(){
     return(
