98567a718fb4617325147423a4e239f94df2f5ec
proyectoAlmayT <alma.beroisa.epet20@gmail.com>
2023-06-20 21:12:39 -0300

    importamos la funcion de planilla de asistencias

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import {Button} from './componentes/button/button.jsx';
 import {input} from './componentes/input/input.jsx';
 import {lista} from './componentes/Lista Desplegable/lista.jsx';
+import {planilla} from './componentes/Planilla Asistencia/planilla.jsx';
 
 function App(){
     return(
