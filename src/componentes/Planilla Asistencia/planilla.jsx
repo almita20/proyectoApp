fa1bc71adbb3fa10fb09c7f605cc4db787854f3e
proyectoAlmayT <alma.beroisa.epet20@gmail.com>
2023-06-20 21:05:32 -0300

    estructura del thead

--- src/componentes/Planilla Asistencia/planilla.jsx
+++ src/componentes/Planilla Asistencia/planilla.jsx
@@ -2,6 +2,13 @@ export function planilla(props){
     return(
             <div className="planilla">
             <form action="/guardar_asistencia" method="post">
+            <table>
+            <thead>
+            <tr>
+            <th>Nombre del Alumno</th>
+            <th>Asistencia</th>
+            </tr>
+            </thead>
             
             </div>
     )
