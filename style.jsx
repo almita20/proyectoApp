23e2ec6799c50a54f8d85ffeacb863ec50183ed8
proyectoAlmayT <tobiassgomez442@gmail.com>
2023-11-07 22:16:15 -0300

    style

---
+++ style.jsx
@@ -0,0 +1,31 @@
+
+const CANCELADO = () =>{
+background: "#f44336";
+Color: "white"; 
+Paddin:"20px";
+text: "center";
+font: "18px";
+border: "2px solid #d32f2f";
+border: "5px";
+width: "300px";
+margin: "0 auto";
+}
+
+const ERROR = () =>{
+background: "red";
+padding: "20px";
+border: "2px solid darkred";
+text: "center";
+width: "300px";
+margin: "0 auto";
+}
+    
+const ERRORSEÑAL = () =>{
+        
+  background: "#ff0000";
+  color: "#ffffff";
+  padding: "10px";
+  border: "1px solid #ff0000";
+  border: "5px";
+  text: "center";
+}
